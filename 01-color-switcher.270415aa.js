11fa569055531ca4e9da7c5115be1b0aa21ba1b4
YanaBytkova <129295681+YanaBytkova@users.noreply.github.com>
2023-08-31 21:30:02 +0000

    Deploying to gh-pages from @ YanaBytkova/goit-js-hw-09@77c4f0a91f00b927c15e12b292ffd91afdbcfec7 🚀

---
+++ 01-color-switcher.270415aa.js
@@ -0,0 +1,2 @@
+const e=document.querySelector("[data-start]"),t=document.querySelector("[data-stop]"),a=document.querySelector("body");let r=null;e.addEventListener("click",(()=>{r=setInterval((()=>{let e=`#${Math.floor(16777215*Math.random()).toString(16).padStart(6,0)}`;a.style.backgroundColor=e}),1e3),e.disabled=!0})),t.addEventListener("click",(()=>{clearInterval(r),e.disabled=!1}));
+//# sourceMappingURL=01-color-switcher.270415aa.js.map
