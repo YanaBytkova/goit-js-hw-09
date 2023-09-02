748c2a687d7e1ddeffe86a73281387cf9d7ccff4
YanaBytkova <nedavnyaya.yana@gmail.com>
2023-09-02 17:02:53 +0300

    try2

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -6,8 +6,15 @@ const timerBox = document.querySelector('.timer');
 const unitBox = document.querySelector('.field');
 const valueBox = document.querySelector('.value');
 const labelBox = document.querySelector('.label');
+const infoDay = document.querySelector('[data-days]');
+const infoHour = document.querySelector('[data-hours]');
+const infoMinute = document.querySelector('[data-minutes]');
+const infoSecond = document.querySelector('[data-seconds]');
 const btnStart = document.querySelector('[data-start]');
 const inputDate = document.querySelector('#datetime-picker');
+
+btnStart.disabled = true;
+errorMessage = "Please choose a date in the future";
 
 const chooseDate = () => {
     const options = {
@@ -20,21 +27,66 @@ const chooseDate = () => {
         },
       };
     const yourDate = inputDate.flatpickr('input#datetime-picker', options);
-  
+    const currentDate = new Date();
+    const choosingDate = new Date(yourDate.selectedDates);
+    const ms = choosingDate - currentDate;
+    console.log(ms);
+    return ms;
     timerId = setInterval(() => {
-        console.log(yourDate.selectedDates);
+        // console.log(yourDate.selectedDates);
+        // console.log(currentDate);
         if (yourDate.selectedDates.length > 0) {
-            clearInterval(timerId);
-            return yourDate.selectedDates;
+            clearInterval(timerId);}
+
+      }, 3000); }
+  
+      chooseDate();
+
+      const promise = new Promise((resolve, reject) => {
+        
+        setTimeout(() => {
+          console.log("ms = ", chooseDate()); 
+          if (chooseDate() > 0) {
+            resolve(chooseDate());
+          } else {
+            reject(errorMessage);
           }
-      }, 3000); 
+        }, 5000);
+      });
       
-}
- chooseDate();
+      promise.then( ( ms ) => {
+          btnStart.disabled = false;
+          convertMs(ms);
+      }, error => {
+        window.alert(error);
+      });
 
-
-const startTimeCounter = () => {
-    // console.log(inputDate.value);
-}
+      function convertMs(ms) {
+        // Number of milliseconds per unit of time
+        const second = 1000;
+        const minute = second * 60;
+        const hour = minute * 60;
+        const day = hour * 24;
+      
+        // Remaining days
+        const days = Math.floor(ms / day);
+        // Remaining hours
+        const hours = Math.floor((ms % day) / hour);
+        // Remaining minutes
+        const minutes = Math.floor(((ms % day) % hour) / minute);
+        // Remaining seconds
+        const seconds = Math.floor((((ms % day) % hour) % minute) / second);
+        // console.log("result:", days, hours, minutes, seconds);
+        infoDay.textContent = days;
+        infoHour.textContent = hours;
+        infoMinute.textContent = minutes;
+        infoSecond.textContent = seconds;
+        return { days, hours, minutes, seconds };
+      }
+    const startTimeCounter = () => {
+    setInterval(() => {
+    convertMs(chooseDate());
+    }, 1000);
+    }   
 
  btnStart.addEventListener('click', startTimeCounter);
